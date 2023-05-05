03ebb7f86d9687aba677d646c4be16e2055170de
jiaoshijie <jiao1751959040@gmail.com>
2023-05-05 19:08:55 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { render } from './markdownit.ts';
+import { parse } from 'https://deno.land/std@0.159.0/flags/mod.ts';
+
+// -i(input_file) -o(output_file)
+const __args = parse(Deno.args)
+
+const input_path = __args['i']
+if (input_path === undefined) {
+  console.log("ERROR: input file must be specified")
+  Deno.exit(1)
+}
+
+try {
+  const input_info = await Deno.stat(input_path)
+} catch (error) {
+  if (error instanceof Deno.errors.NotFound) {
+    console.log("ERROR: input file doesn't exist.")
+    Deno.exit(1)
+  } else {
+    throw error
+  }
+}
+
+let output_path = __args['o']
+
+if (output_path === undefined) {
+  output_path = await Deno.makeTempFile({prefix: "geg_", suffix: ".html"})
+}
+
+let index_before = await Deno.readTextFile("./resources/index_before.html")
+let content = render(await Deno.readTextFile(input_path))
+let index_after = await Deno.readTextFile("./resources/index_after.html")
+
+let new_html = index_before + content + index_after
+
+try {
+  await Deno.writeTextFile(output_path, new_html)
+  console.log(output_path)
+} catch (error) {
+  throw error
+}
---
+++ src/markdownit.ts
@@ -0,0 +1,141 @@
+import { hashCode, uniqueIdGen } from './utils.ts';
+import { parse } from 'https://deno.land/std@0.159.0/flags/mod.ts';
+import { default as highlight } from 'https://cdn.skypack.dev/highlight.js@11.6.0';
+// @deno-types="https://cdn.skypack.dev/@types/markdown-it@12.2.3?dts"
+import MarkdownIt from 'https://esm.sh/markdown-it@13.0.1?no-dts';
+// @deno-types="./markdownit_plugin.d.ts"
+import { default as MarkdownItEmoji } from 'https://esm.sh/markdown-it-emoji@2.0.2?no-dts';
+// @deno-types="./markdownit_plugin.d.ts"
+import { default as MarkdownItFootnote } from 'https://esm.sh/markdown-it-footnote@3.0.3?no-dts';
+// @deno-types="./markdownit_plugin.d.ts"
+import { default as MarkdownItTaskLists } from 'https://esm.sh/markdown-it-task-lists@2.1.1?no-dts';
+// @deno-types="./markdownit_plugin.d.ts"
+import { default as MarkdownItTexmath } from 'https://esm.sh/markdown-it-texmath@1.0.0?no-dts';
+import Katex from 'https://esm.sh/katex@0.16.3?no-dts';
+
+const __args = parse(Deno.args);
+
+const md = new MarkdownIt('default', {
+  html: true,
+  typographer: true,
+  linkify: true,
+  langPrefix: 'language-',
+  highlight: __args['syntax'] && ((code, language) => {
+    if (language && highlight.getLanguage(language)) {
+      try {
+        return highlight.highlight(code, { language }).value;
+      } catch {
+        return code;
+      }
+    }
+
+    return '';
+  }),
+}).use(MarkdownItEmoji)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItTaskLists, { enabled: false, label: true })
+  .use(MarkdownItTexmath, {
+    engine: Katex,
+    delimiters: ['dollars', 'gitlab'],
+    katexOptions: { macros: { '\\R': '\\mathbb{R}' } },
+  });
+
+md.renderer.rules.link_open = (tokens, idx, options) => {
+  const token = tokens[idx];
+  const href = token.attrGet('href');
+
+  if (href && href.startsWith('#')) {
+    token.attrSet('onclick', `location.hash='${href}'`);
+  }
+
+  token.attrSet('href', 'javascript:return');
+
+  return md.renderer.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.heading_open = (tokens, idx, options) => {
+  tokens[idx].attrSet(
+    'id',
+    tokens[idx + 1].content
+      .trim()
+      .split(' ')
+      .filter((a) => a)
+      .join('-')
+      .replace(/[^a-z0-9-]/gi, '')
+      .toLowerCase(),
+  );
+
+  return md.renderer.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.math_block = (() => {
+  const math_block = md.renderer.rules.math_block!;
+
+  return (tokens, idx, options, env, self) => {
+    return `
+      <div
+        data-line-begin="${tokens[idx].attrGet('data-line-begin')}"
+      >
+        ${math_block(tokens, idx, options, env, self)}
+      </div>
+    `;
+  };
+})();
+
+md.renderer.rules.math_block_eqno = (() => {
+  const math_block_eqno = md.renderer.rules.math_block_eqno!;
+
+  return (tokens, idx, options, env, self) => {
+    return `
+      <div
+        data-line-begin="${tokens[idx].attrGet('data-line-begin')}"
+      >
+        ${math_block_eqno(tokens, idx, options, env, self)}
+      </div>
+    `;
+  };
+})();
+
+md.renderer.rules.fence = (() => {
+  const fence = md.renderer.rules.fence!;
+  const escapeHtml = md.utils.escapeHtml;
+  const regex = new RegExp(
+    /^(flowchart|sequenceDiagram|gantt|classDiagram|stateDiagram|pie|journey|C4Context|erDiagram|requirementDiagram|gitGraph)/,
+  );
+
+  return (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const content = token.content.trim();
+
+    if (regex.test(content)) {
+      return `
+        <div
+          class="mermaid"
+          data-line-begin="${token.attrGet('data-line-begin')}"
+        >
+          <div
+            id="graph-mermaid-${env.genId(hashCode(content))}"
+            data-graph="mermaid"
+            data-graph-definition="${escapeHtml(content)}"
+          >
+            <div class="loader"></div>
+          </div>
+        </div>
+      `;
+    }
+
+    return fence(tokens, idx, options, env, self);
+  };
+})();
+
+export function render(markdown: string) {
+  const tokens = md.parse(markdown, {});
+
+  tokens.forEach((token) => {
+    if (token.map && token.level === 0) {
+      token.attrSet('data-line-begin', String(token.map[0] + 1));
+    }
+  });
+
+  return md.renderer.render(tokens, md.options, { genId: uniqueIdGen() });
+}
